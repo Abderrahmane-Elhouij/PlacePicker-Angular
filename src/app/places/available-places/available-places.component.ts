3af81945aa504fc63fa9f74f9643ec1f8a6bf872
Abderrahmane El-houij <abdoel132003@gmail.com>
2024-11-27 21:46:16 +0100

    add initial structure for PlacePicker application with user and available places components

---
+++ src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,35 @@
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent {
+  places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient)
+  private destroyRef = inject(DestroyRef)
+
+  ngOnInit() {
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(map((resData) => resData.places))
+      .subscribe({
+        next: (places) => {
+          this.places.set(places)
+        }
+      })
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe()
+    })
+  }
+}
