11335ac8fc02b890bdbbc1449055ccc5cf105f27
Abderrahmane El-houij <abdoel132003@gmail.com>
2024-11-28 15:07:10 +0100

    add error handling and modal for user place operations

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { Place } from './place.model';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, tap, throwError } from 'rxjs';
+import { ErrorService } from '../shared/error.service';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,7 @@ import { catchError, map, tap, throwError } from 'rxjs';
 export class PlacesService {
   private userPlaces = signal<Place[]>([]);
   private httpClient = inject(HttpClient);
+  private errorService = inject(ErrorService)
 
   loadedUserPlaces = this.userPlaces.asReadonly();
 
@@ -29,7 +31,7 @@ export class PlacesService {
 
   addPlaceToUserPlaces(place: Place) {
     const prevPlaces = this.userPlaces()
-    
+
     if (!prevPlaces.some((p) => p.id === place.id)) {
       this.userPlaces.set([...prevPlaces, place])
     }
@@ -39,12 +41,28 @@ export class PlacesService {
     }).pipe(
       catchError((error) => {
         this.userPlaces.set(prevPlaces)
+        this.errorService.showError('Failed to store selected place.')
         return throwError(() => new Error('Failed to store selected place.'))
       })
     )
   }
 
-  removeUserPlace(place: Place) { }
+  removeUserPlace(place: Place) {
+    const prevPlaces = this.userPlaces()
+
+    if (prevPlaces.some((p) => p.id === place.id)) {
+      this.userPlaces.set(prevPlaces.filter((p)=> p.id !== place.id))
+    }
+
+    return this.httpClient.delete('http://localhost:3000/user-places/' + place.id)
+      .pipe(
+        catchError((error) => {
+          this.userPlaces.set(prevPlaces)
+          this.errorService.showError('Failed to remove the selected place.')
+          return throwError(() => new Error('Failed to remove the selected place.'))
+        })
+      )
+  }
 
   private fetchPlaces(url: string, errorMessage: string) {
     return this.httpClient
